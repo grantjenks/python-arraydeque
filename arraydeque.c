7aafb5e79de33d9f3c534df8e1b0f34d0a962d92
Grant Jenks <grant.jenks@gmail.com>
2025-02-02 00:02:53 -0800

    Bump version to 1.3.0

--- arraydeque.c
+++ arraydeque.c
@@ -4,7 +4,7 @@
 #include <stddef.h>  /* for offsetof */
 
 #ifndef ARRAYDEQUE_VERSION
-#define ARRAYDEQUE_VERSION "1.2.8"
+#define ARRAYDEQUE_VERSION "1.3.0"
 #endif
 
 /* The ArrayDeque object structure. */
